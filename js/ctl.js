8f93aa11d7366a9b61081aad1636a9c26cdb3ea8
Vladimir Goncharov <devel@viruzzz.org>
2019-11-24 23:41:08 +0300

    Few improvements

--- js/ctl.js
+++ js/ctl.js
@@ -10,56 +10,43 @@ var config = {
 
 
 var updateLoop;
-var scoreboard_data;
+var scoreboard_data={};
 firebase.initializeApp(config);
 db=firebase.firestore();
-scoreboard_collection=db.collection('basketball')
-scoreboard_query=scoreboard_collection.where("token",'==',token).limit(1)
+scoreboard_collection=db.collection('volleyball')
+scoreboard_query=scoreboard_collection.doc(token)
 scoreboard_query.onSnapshot(
-  function(querySnapshot){
-    querySnapshot.docs.map(
-      function(documentSnapshot){
-        scoreboard_data=documentSnapshot.data();
-        console.log(scoreboard_data);
-        $('.away_team').html(scoreboard_data['away_team'])
-        var disp=scoreboard_data['show'];
-        if(disp == false) disp=0; else if(disp == true) disp=1;
+  function(documentSnapshot){
+    scoreboard_data=documentSnapshot.data();
+    console.log(scoreboard_data);
+    $('.away_team').html(scoreboard_data['away_team'])
+    var disp=scoreboard_data['show'];
+    if(disp == false) disp=0; else if(disp == true) disp=1;
 
-        $(".show-select").removeClass("btn-info");
-        if(disp == 0){
-          $(".show-select[data-val='0']").addClass("btn-info");
-        }else if(disp == 1){
-          $(".show-select[data-val='1']").addClass("btn-info");
-        }else if(disp == 2){
-          $(".show-select[data-val='2']").addClass("btn-info");
-        }
+    $(".show-select").removeClass("btn-info");
+    if(disp == 0){
+      $(".show-select[data-val='0']").addClass("btn-info");
+    }else if(disp == 1){
+      $(".show-select[data-val='1']").addClass("btn-info");
+    }else if(disp == 2){
+      $(".show-select[data-val='2']").addClass("btn-info");
+    }
 
-        //$("#show").prop("checked",(scoreboard_data['show']));
-        $('#away_score').html(scoreboard_data['away_score'])
-        $('#away_fouls').html(scoreboard_data['away_fouls'])
-        $('.home_team').html(scoreboard_data['home_team'])
-        $('#in_home_team').val(scoreboard_data['home_team'])
-        $('#in_away_team').val(scoreboard_data['away_team'])
-        $('#col_home_team').val(scoreboard_data['home_color'])
-        $('#col_away_team').val(scoreboard_data['away_color'])
-        $('#home_score').html(scoreboard_data['home_score'])
-        $('#home_fouls').html(scoreboard_data['home_fouls'])
-        $('#period').html(scoreboard_data['current_period'])
-      });
+    //$("#show").prop("checked",(scoreboard_data['show']));
+    $('#away_score').html(scoreboard_data['away_score'])
+    $('#away_fouls').html(scoreboard_data['away_fouls'])
+    $('.home_team').html(scoreboard_data['home_team'])
+    $('#in_home_team').val(scoreboard_data['home_team'])
+    $('#in_away_team').val(scoreboard_data['away_team'])
+    $('#col_home_team').val(scoreboard_data['home_color'])
+    $('#col_away_team').val(scoreboard_data['away_color'])
+    $('#home_score').html(scoreboard_data['home_score'])
+    $('#home_fouls').html(scoreboard_data['home_fouls'])
+    $('#period').html(scoreboard_data['current_period'])
   });
 
 function update_db(data){
-  console.log("Update1 ",data);
-  scoreboard_query.get().then(
-    function(querySnapshot){
-      querySnapshot.docs.map(
-        function(documentSnapshot){
-          db.collection('basketball').doc(documentSnapshot.id).update(data)});
-    }).catch(
-      function(error) {
-        console.log("Failed!", error);
-      }
-    );
+  scoreboard_query.update(data);
 }
 $(document).ready(function(){
   $(".show-select").click(function(){
@@ -99,11 +86,17 @@ $(document).ready(function(){
     update_db(update);
   });
   $(".reset-btn").click(function(){
-    update_db({home_score:0,
+    scoreboard_collection.doc(token).set({
+      home_score:0,
       home_fouls:0,
       away_score:0,
       away_fouls:0,
-      current_period:1});
+      current_period:1,
+      away_team: $("#in_away_team").val(),
+      away_color: $("#col_away_team").val(),
+      home_team: $("#in_home_team").val(),
+      home_color: $("#col_home_team").val(),
+    });
   });
   $(".period-btn").click(function(){
     var button=$(this);
--- js/scoreboard.js
+++ js/scoreboard.js
@@ -12,38 +12,33 @@ var updateLoop;
 var scoreboard_data={};
 firebase.initializeApp(config);
 db=firebase.firestore();
-scoreboard_collection=db.collection('basketball')
-scoreboard_query=scoreboard_collection.where("token",'==',token).limit(1)
+scoreboard_collection=db.collection('volleyball')
+scoreboard_query=scoreboard_collection.doc(token)
 scoreboard_query.onSnapshot(
-  function(querySnapshot){
-    querySnapshot.docs.map(
-      function(documentSnapshot){
-        var pdata=scoreboard_data;
-        scoreboard_data=documentSnapshot.data();
-        if(pdata['show'] != scoreboard_data['show']){
-          if(scoreboard_data.show == 2){
-            $(".big-table").animate({"left":80},1000)
-          }else{
-            $(".big-table").animate({"left":-800},1000)
-          }
+  function(documentSnapshot){
+    var pdata=scoreboard_data;
+    scoreboard_data=documentSnapshot.data();
+    if(pdata['show'] != scoreboard_data['show']){
+      if(scoreboard_data.show == 2){
+        $(".big-table").animate({"left":80},1000)
+      }else{
+        $(".big-table").animate({"left":-800},1000)
+      }
 
-          if(scoreboard_data.show==1){
-            $("table#top").animate({"margin-top":0},1000)
-          }else{
-            $("table#top").animate({"margin-top":-80},1000)
-          }
-          console.log(scoreboard_data.show);
-        }
-        console.log("ds ",scoreboard_data);
-        $('.away_team').html(scoreboard_data['away_team'])
-        $('.away_score').html(scoreboard_data['away_score'])
-        $('.away_fouls').html(scoreboard_data['away_fouls'])
-        $(".away_color").css("background-color",scoreboard_data['away_color'])
-        $('.home_team').html(scoreboard_data['home_team'])
-        $('.home_score').html(scoreboard_data['home_score'])
-        $('.home_fouls').html(scoreboard_data['home_fouls'])
-        $(".home_color").css("background-color",scoreboard_data['home_color'])
-        $('.timeval').html(scoreboard_data['current_period'])
-      });
+      if(scoreboard_data.show==1){
+        $("table#top").animate({"margin-top":0},1000)
+      }else{
+        $("table#top").animate({"margin-top":-80},1000)
+      }
+    }
+    $('.away_team').html(scoreboard_data['away_team'])
+    $('.away_score').html(scoreboard_data['away_score'])
+    $('.away_fouls').html(scoreboard_data['away_fouls'])
+    $(".away_color").css("background-color",scoreboard_data['away_color'])
+    $('.home_team').html(scoreboard_data['home_team'])
+    $('.home_score').html(scoreboard_data['home_score'])
+    $('.home_fouls').html(scoreboard_data['home_fouls'])
+    $(".home_color").css("background-color",scoreboard_data['home_color'])
+    $('.timeval').html(scoreboard_data['current_period'])
   });
 
